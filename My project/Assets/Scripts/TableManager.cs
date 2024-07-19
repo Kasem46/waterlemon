366773ecd65c5b5bffafd377d6b972f3688266c6
Archived2706 <114086529+Archived2706@users.noreply.github.com>
2024-07-19 16:25:03 -0400

    Tasbles has  people

--- My project/Assets/Scripts/TableManager.cs
+++ My project/Assets/Scripts/TableManager.cs
@@ -6,25 +6,27 @@ using UnityEngine.SceneManagement;
 public class TableManager : MonoBehaviour
 {
 
-    private GameManager manager;
+    private int peopleAtTable;
+    public GenericNPC[] NPCS;
+    private GameManager thing;
 
-    private GenericNPC personA;
-    private GenericNPC personB;
-    private GenericNPC personC;
 
     // Start is called before the first frame update
     void Start()
     {
+        thing = GameObject.Find("GameManager").GetComponent<GameManager>();
         //randomly populate table with people
+        peopleAtTable = Random.Range(1,4);
+        NPCS = new GenericNPC[peopleAtTable];
+        for (int i  = 0; i < NPCS.Length; i++){
+            NPCS[i] = thing.NPCs[Random.Range(0,18)];
+        }
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         
-    }
-
-    void onMouseDown() {
-        SceneManager.LoadScene("Plotting");
     }
 }
