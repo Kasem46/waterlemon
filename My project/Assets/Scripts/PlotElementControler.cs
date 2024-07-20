07580e2c52357dd231f458203f974e1305ae57e3
Dexo55 <Dexoh53@gmail.com>
2024-07-20 10:03:50 -0400

    Reading Required

--- My project/Assets/Scripts/PlotElementControler.cs
+++ My project/Assets/Scripts/PlotElementControler.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlotElementControler : MonoBehaviour {
     public GameObject book;
+    private GameManager[] test;
+    private GenericNPC[] NPCs;
+    private GameManager manager;
+    private Text FactionText;
     private GameObject[] FactionsArray1;
     private GameObject[] FactionsArray2;
+    private GameObject[] FactionsArray3;
     private GameObject[] Buttons;
     private GameObject[] Miror;
     //Show/Hide main buttons    
@@ -33,12 +39,16 @@ public class PlotElementControler : MonoBehaviour {
         foreach(GameObject go in FactionsArray2){
             go.SetActive (false);
         }
+        foreach(GameObject go in FactionsArray3){
+            go.SetActive (false);
+        }
         showMainButtons();
     }
     private void setFactionBook(){
         book.SetActive(false);
         FactionsArray1 = GameObject.FindGameObjectsWithTag ("FactionPage1");
         FactionsArray2 = GameObject.FindGameObjectsWithTag ("FactionPage2");
+        FactionsArray3 = GameObject.FindGameObjectsWithTag ("FactionPage3");
     }
     //Control the pages of the Faction Book
     public void factionPage1(){
@@ -48,6 +58,9 @@ public class PlotElementControler : MonoBehaviour {
         foreach(GameObject go in FactionsArray1){
             go.SetActive (true);
         }
+        foreach(GameObject go in FactionsArray3){
+            go.SetActive (false);
+        }
     }
     public void factionPage2(){
         foreach(GameObject go in FactionsArray1){
@@ -55,6 +68,24 @@ public class PlotElementControler : MonoBehaviour {
         }
         foreach(GameObject go in FactionsArray2){
             go.SetActive (true);
+        }
+        foreach(GameObject go in FactionsArray3){
+            go.SetActive (false);
+        }
+    }
+    public void factionPage3(){
+        foreach(GameObject go in FactionsArray1){
+            go.SetActive (false);
+        }
+        foreach(GameObject go in FactionsArray2){
+            go.SetActive (false);
+        }
+        foreach(GameObject go in FactionsArray3){
+            go.SetActive (true);
+        }
+        for (int i = 1; i < 19; i++){
+            FactionText = GameObject.Find(i.ToString()).GetComponent<Text> ();
+            FactionText.text = "- " + NPCs[i-1].getName() + ". Influence: " + NPCs[i-1].getInfluence(); 
         }
     }
 
@@ -75,9 +106,12 @@ public class PlotElementControler : MonoBehaviour {
     private void setMiror(){
         Miror = GameObject.FindGameObjectsWithTag ("Miror");
     }
-    
+
     // Start is called before the first frame update
     void Start(){
+        test = FindObjectsOfType<GameManager>();
+        manager = test[0];
+        NPCs = manager.getNPCArray();
         setMainButtons();
         setFactionBook();
         setMiror();
